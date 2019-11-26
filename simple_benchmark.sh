5af88f1f3994646531ae350ebe47cc4b69ede22f
Jakob Otto <jakob.otto@haw-hamburg.de>
2019-11-26 22:26:58 +0100

    Tweak benchmark and R scripts

--- simple_benchmark.sh
+++ simple_benchmark.sh
@@ -9,12 +9,42 @@ function update_caf_application_ini() {
   
 }
 
-file_name="evaluation/simple_streaming"
-echo "num_serializing_workers, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10" > ${file_name}.out
+file_name="evaluation/4-deserializing-workers"
+echo "num_workers, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11," > ${file_name}.csv
 for i in {0..4}; do
   echo "Starting benchmark deserializing_workers = 4 and serializing_workers = $i:"
   update_caf_application_ini 4 $i
-  ./release/simple_streaming -mnetBench >> ${file_name}.out 2> ${file_name}.err
+  ./release/simple_streaming -mnetBench >> ${file_name}.csv 2> ${file_name}.err
+  echo ""
+  echo ""
+done
+
+file_name="evaluation/4-serializing-workers"
+echo "num_workers, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11," > ${file_name}.csv
+for i in {0..4}; do
+  echo "Starting benchmark deserializing_workers = $i and serializing_workers = 4:"
+  update_caf_application_ini $i 4
+  ./release/simple_streaming -mnetBench >> ${file_name}.csv 2> ${file_name}.err
+  echo ""
+  echo ""
+done
+
+file_name="evaluation/0-serializing-workers"
+echo "num_workers, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11," > ${file_name}.csv
+for i in {0..4}; do
+  echo "Starting benchmark deserializing_workers = $i and serializing_workers = 4:"
+  update_caf_application_ini $i 0
+  ./release/simple_streaming -mnetBench >> ${file_name}.csv 2> ${file_name}.err
+  echo ""
+  echo ""
+done
+
+file_name="evaluation/0-deserializing-workers"
+echo "num_workers, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11," > ${file_name}.csv
+for i in {0..4}; do
+  echo "Starting benchmark deserializing_workers = 4 and serializing_workers = $i:"
+  update_caf_application_ini 4 $i
+  ./release/simple_streaming -mnetBench >> ${file_name}.csv 2> ${file_name}.err
   echo ""
   echo ""
 done
