b9786f656d3825578c6a60555ec68b0833cde77b
Jakob Otto <jakob.otto@haw-hamburg.de>
2019-12-03 16:15:25 +0100

    Add bytewise benchmarking

--- evaluation/interface_load.sh
+++ evaluation/interface_load.sh
@@ -10,11 +10,6 @@ direction=$2
 seconds=$3
 file_name=$4
 
-header="value_0"
-for (( c=1; c<=$3; c++ )); do
-  header="${header}, value_${c}"
-done
-
 declare -a arr
 F="/sys/class/net/${iface}/statistics/${direction}"
 BPS=0
@@ -23,7 +18,6 @@ for (( c=1; c<=$seconds; c++ )); do
   arr+=($BPS)
 done
 
-echo $header > $file_name
 for val in "${arr[*]}"; do
   printf "%d, " $val >> $file_name
 done
--- simple_benchmark.sh
+++ simple_benchmark.sh
@@ -8,59 +8,60 @@ function update_caf_application_ini() {
    
 }
 
-function init_file() {
-  echo "serializer, deserializer, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11," > $1.csv
+function init_files() {
+  echo "serializer, deserializer, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10," > $1.csv
+  rm -rf ${1}-if-load.out
 }
 
 output_folder="evaluation/out"
 mkdir -p $output_folder
 
-file_name="${output_folder}/4serializer-x-deserializer"
-init_file $file_name
+file_name="${output_folder}/4-ser-x-deser"
+init_files $file_name
 for i in {0..4}; do
   echo "[4:$i]:"
   update_caf_application_ini 4 $i
-  ./release/simple_streaming -mnetBench >> ${file_name}.csv 2> ${file_name}.err
-  echo ""
-  echo ""
+  sh evaluation/interface_load.sh lo rx_bytes 12 ${file_name}-if-load.out &
+  ./release/simple_streaming -i11 -mnetBench >> ${file_name}.csv 2> ${file_name}.err
+  sleep 2
 done
 
-file_name="${output_folder}/0serializer-x-deserializer"
-init_file $file_name
+file_name="${output_folder}/0-ser-x-deser"
+init_files $file_name
 for i in {0..4}; do
-    echo "[0:$i]:"
+  echo "[0:$i]:"
   update_caf_application_ini 0 $i
-  ./release/simple_streaming -mnetBench >> ${file_name}.csv 2> ${file_name}.err
-  echo ""
-  echo ""
+  sh evaluation/interface_load.sh lo rx_bytes 12 ${file_name}-if-load.out &
+  ./release/simple_streaming -i11 -mnetBench >> ${file_name}.csv 2> ${file_name}.err
+  sleep 2
 done
 
-file_name="${output_folder}/x-serializer-4-deserializer"
-init_file $file_name
+file_name="${output_folder}/x-ser-4-deser"
+init_files $file_name
 for i in {0..4}; do
   echo "[$i:4]:"
   update_caf_application_ini $i 4
-  ./release/simple_streaming -mnetBench >> ${file_name}.csv 2> ${file_name}.err
-  echo ""
-  echo ""
+  sh evaluation/interface_load.sh lo rx_bytes 12 ${file_name}-if-load.out &
+  ./release/simple_streaming -i11 -mnetBench >> ${file_name}.csv 2> ${file_name}.err
+  sleep 2
 done
 
-file_name="${output_folder}/x-serializer-0-deserializer"
-init_file $file_name
+file_name="${output_folder}/x-ser-0-deser"
+init_files $file_name
 for i in {0..4}; do
   echo "[$i:0]:"
   update_caf_application_ini $i 0
-  ./release/simple_streaming -mnetBench >> ${file_name}.csv 2> ${file_name}.err
-  echo ""
-  echo ""
+  sh evaluation/interface_load.sh lo rx_bytes 12 ${file_name}-if-load.out &
+  ./release/simple_streaming -i11 -mnetBench >> ${file_name}.csv 2> ${file_name}.err
+  sleep 2
 done
 
-file_name="${output_folder}/io-x-deserializer"
-init_file $file_name
+file_name="${output_folder}/io-x-deser"
+init_files $file_name
 for i in {0..4}; do
   echo "[$i:0]:"
   update_caf_application_ini 0 $i
-  ./release/simple_streaming -mioBench >> ${file_name}.csv 2> ${file_name}.err
-  echo ""
-  echo ""
+  sh evaluation/interface_load.sh lo rx_bytes 12 ${file_name}-if-load.out &
+  ./release/simple_streaming -i11 -mioBench >> ${file_name}.csv 2> ${file_name}.err
+  sleep 2
 done
