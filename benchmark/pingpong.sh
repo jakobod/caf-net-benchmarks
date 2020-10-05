58785e6721c42c75154d0fddca1f8ee466b8e1af
Jakob Otto <jakob.otto@haw-hamburg.de>
2020-10-05 21:48:00 +0200

    refactor pingpong benchmark

--- benchmark/pingpong.sh
+++ benchmark/pingpong.sh
@@ -13,59 +13,40 @@ function init_file() {
 
 echo "-- netBenchmark ---------------------------------------------------------"
 
-out_file="evaluation/out/pingpong-net-variable-size"
-init_file ping_size ${out_file}
-ping_size=1
-while [ $ping_size -lt 40000 ]; do
-  echo "------------------------ ${ping_size} Bytes ---------------------------"
-  printf "${ping_size}, " >> ${out_file}.out
-  while : ; do
-    ./release/pingpong_tcp -mnetBench -s$ping_size >> ${out_file}.out 2> ${out_file}.err
-    [[ $? != 0 ]] || break # if program exited with error rerun it.
+out_file="evaluation/out/pingpong-tcp-net-num-pings"
+echo "-- pingpong-tcp-net-num-pings --------------------------------------"
+init_file num_pings ${out_file}
+num_pings=1024
+while [ $num_pings -le 600000 ]; do
+  echo "-- ${num_pings} pings -----------------------------------------------"
+  printf "${num_pings}, " >> ${out_file}.out
+  for i in 0 1 2 3 4 5 6 7 8 9; do
+    while : ; do
+      ./release/pingpong_tcp -mnetBench -p$num_pings >> ${out_file}.out 2> ${out_file}.err
+      [[ $? != 0 ]] || break # if program exited with error rerun it.
+    done;
   done;
-  ping_size=$((ping_size*2))
+  echo "" >> ${out_file}.out
+  num_pings=$((num_pings*2))
 done;
 
-
-out_file="evaluation/out/pingpong-net-variable-remote-nodes"
-init_file remote_nodes ${out_file}
-remote_nodes=1
-while [ $remote_nodes -le 64 ]; do
-  echo "---------------------- ${remote_nodes} nodes --------------------------"
-  printf "${remote_nodes}, " >> ${out_file}.out
-  while : ; do
-    ./release/pingpong_tcp -mnetBench -n$remote_nodes >> ${out_file}.out 2> ${out_file}.err
-    [[ $? != 0 ]] || break # if program exited with error rerun it.
-  done;
-  remote_nodes=$((remote_nodes+1))
-done;
 
 
 echo "-- ioBenchmark ----------------------------------------------------------"
 
-out_file="evaluation/out/pingpong-io-variable-size"
-init_file ping_size ${out_file}
-ping_size=1
-while [ $ping_size -lt 40000 ]; do
-  echo "------------------------ ${ping_size} Bytes ------------------------"
-  printf "${ping_size}, " >> ${out_file}.out
-  while : ; do
-    ./release/pingpong_tcp -mioBench -s$ping_size >> ${out_file}.out 2> ${out_file}.err
-    [[ $? != 0 ]] || break # if program exited with error rerun it.
+out_file="evaluation/out/pingpong-tcp-io-num-pings"s
+echo "-- pingpong-tcp-io-num-pings --------------------------------------"
+init_file num_pings ${out_file}
+num_pings=1024
+while [ $num_pings -le 600000 ]; do
+  echo "-- ${num_pings} pings -----------------------------------------------"
+  printf "${num_pings}, " >> ${out_file}.out
+  for i in 0 1 2 3 4 5 6 7 8 9; do
+    while : ; do
+      ./release/pingpong_tcp -mioBench -p$num_pings >> ${out_file}.out 2> ${out_file}.err
+      [[ $? != 0 ]] || break # if program exited with error rerun it.
+    done;
   done;
-  ping_size=$((ping_size*2))
-done;
-
-
-out_file="evaluation/out/pingpong-io-variable-remote-nodes"
-init_file remote_nodes ${out_file}
-remote_nodes=1
-while [ $remote_nodes -le 64 ]; do
-  echo "---------------------- ${remote_nodes} nodes -----------------------"
-  printf "${remote_nodes}, " >> ${out_file}.out
-  while : ; do
-    ./release/pingpong_tcp -mioBench -n$remote_nodes >> ${out_file}.out 2> ${out_file}.err
-    [[ $? != 0 ]] || break # if program exited with error rerun it.
-  done;
-  remote_nodes=$((remote_nodes+1))
+  echo "" >> ${out_file}.out
+  num_pings=$((num_pings*2))
 done;
