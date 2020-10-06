8c9da58c81a63ab536e467659de598e74d9b63e0
Jakob Otto <jakob.otto@haw-hamburg.de>
2020-10-06 09:17:52 +0200

    log-scales in pingpong plot

--- evaluation/pingpong_tcp.R
+++ evaluation/pingpong_tcp.R
@@ -26,7 +26,7 @@ ppdf$upper <- ppdf$avg + ppdf$sdev
 ppdf$lower <- ppdf$avg - ppdf$sdev
 
 
-pp_plot <- ggplot(ppdf, aes(x=num_pings, y=avg, color=proto)) +
+pp_plot_log <- ggplot(ppdf, aes(x=num_pings, y=avg, color=proto)) +
   geom_line() + # size=0.8) +
   geom_point(aes(shape=proto), size = 1, stroke=0.6) +
   geom_errorbar(
@@ -61,10 +61,42 @@ pp_plot <- ggplot(ppdf, aes(x=num_pings, y=avg, color=proto)) +
   ggtitle("Pingpong") +
   labs(x="roundtrips [#]", y="duration [ms]")
 
+pp_plot <- ggplot(ppdf, aes(x=num_pings, y=avg, color=proto)) +
+  geom_line() + # size=0.8) +
+  geom_point(aes(shape=proto), size = 1, stroke=0.6) +
+  geom_errorbar(
+    mapping=aes(
+      ymin=lower,
+      ymax=upper
+    ),
+    width=0.4
+  ) +
+  scale_x_continuous() +
+  scale_y_continuous() +
+  theme_bw() +
+  theme(
+    legend.title=element_blank(),
+    legend.key=element_rect(fill='white'), 
+    legend.background=element_rect(fill="white", colour="black", size=0.25),
+    legend.direction="vertical",
+    legend.justification=c(0, 1),
+    legend.position=c(0,1),
+    legend.box.margin=margin(c(3, 3, 3, 3)),
+    legend.key.height=unit(0.4,"line"),
+    legend.key.size=unit(0.6, 'lines'),
+    text=element_text(size=9),
+    strip.text.x=element_blank()
+  ) +
+  scale_color_brewer(type="qual", palette=6) +
+  scale_fill_brewer(palette="Dark2") +
+  ggtitle("Pingpong") +
+  labs(x="roundtrips [#]", y="duration [ms]")
+
 tikz(file="tikz/pingpong.tikz", sanitize=TRUE, width=3.4, height=2.3)
 pp_plot
 dev.off()
 
 #ggsave("figs/pingpong.pdf", plot=pp_plot, width=3.4, height=2.3)
+ggsave("figs/pingpong-log-scale.pdf", plot=pp_plot_log, width=4, height=3)
 ggsave("figs/pingpong.pdf", plot=pp_plot, width=4, height=3)
 
